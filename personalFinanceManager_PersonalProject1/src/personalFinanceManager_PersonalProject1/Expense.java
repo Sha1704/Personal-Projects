d72b73e76c29245d3360beaac3937a5a2ce53edc
Shalom <shalomadiboshi01@gmail.com>
2024-12-21 00:40:06 -0600

    Created the framework for the main and expense class.

---
+++ personalFinanceManager_PersonalProject1/src/personalFinanceManager_PersonalProject1/Expense.java
@@ -0,0 +1,64 @@
+/*
+ * Created on: Dec 21, 2024
+ *
+ * ULID: <sadibos>
+ * Class: IT 179 
+ */
+package personalFinanceManager_PersonalProject1;
+
+/**
+ * <insert class description here>
+ *
+ * @author <Adiboshi Shalom;
+ *
+ */
+public class Expense
+{
+	private String date, category;
+	private double amount;
+	public Expense(String date, String category, double amount)
+	{
+		this.date = date;
+		this.category = category;
+		this.amount = amount;
+	}
+	public String getDate()
+	{
+		return date;
+	}
+	public void setDate(String date)
+	{
+		this.date = date;
+	}
+	public String getCategory()
+	{
+		return category;
+	}
+	public void setCategory(String category)
+	{
+		this.category = category;
+	}
+	public double getAmount()
+	{
+		return amount;
+	}
+	public void setAmount(double amount)
+	{
+		this.amount = amount;
+	}
+	
+	public void displayExpenses()
+	{
+		//to be implemented
+	}
+	
+	public void calculateSpending()
+	{
+		// to be implemented
+	}
+	
+	public void categorizeSpending()
+	{
+		// to be implemented
+	}
+}
--- personalFinanceManager_PersonalProject1/src/personalFinanceManager_PersonalProject1/Main.java
+++ personalFinanceManager_PersonalProject1/src/personalFinanceManager_PersonalProject1/Main.java
@@ -6,8 +6,11 @@
  */
 package personalFinanceManager_PersonalProject1;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
- * <insert class description here>
+ * <Main class; runs the program and contains all necessary stuff>
  *
  * @author <Adiboshi Shalom;
  *
@@ -20,8 +23,57 @@ public class Main
 	 */
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-		System.out.println("Back to blank");
+		try
+		{
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Welcome to the Personal Finance Manager!");
+		
+		showMenu();
+		int input = scan.nextInt();
+		
+		while (!scan.hasNextInt())
+		{
+			System.out.println("Please enter a number");
+		}
+		while (input != 4)
+		{
+			if (input < 1 || input > 4)
+			{
+				System.out.println("Please enter a number between 1-4: ");
+				input = scan.nextInt();
+			}
+			else
+			{
+				if (input == 1)
+				{
+					// add expense method (expense class)
+				}
+				else if (input == 2)
+				{
+					//view expense method (expense class)
+				}
+				else if (input == 3)
+				{
+					//calculate total spending (expense class)
+				}
+			}
+		}
+		System.out.println("Thank you for using the personal finance manager!");
 	}
-
+	catch (InputMismatchException e)
+	{
+			System.out.println("Input Mismatch (you enterd the wrong data-type) please run the program again. Error message: " + e.getMessage());
+	}
+	}	
+	
+	public static void showMenu()
+	{
+		System.out.println("Please choose one of the following options");
+		System.out.println("1. Add Expense");
+		System.out.println("2. View Expenses");
+		System.out.println("3. Calculate Total Spending");
+		System.out.println("4. Exit");
+		System.out.println("Choose an option:");
+	}
 }
