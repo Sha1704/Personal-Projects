dcd1907f92f8f5303726408ba6f3aa7daa7c9fca
Shalom <shalomadiboshi01@gmail.com>
2024-12-20 22:58:28 -0600

    Initial Commit (no outline yet)

---
+++ personalFinanceManager_PersonalProject1/src/module-info.java
@@ -0,0 +1,12 @@
+/*
+ * Created on: Dec 20, 2024
+ *
+ * ULID: <sadibos>
+ * Class: IT 179 
+ */
+/**
+ * 
+ */
+module personalFinanceManager_PersonalProject1
+{
+}
---
+++ personalFinanceManager_PersonalProject1/src/personalFinanceManager_PersonalProject1/Main.java
@@ -0,0 +1,27 @@
+/*
+ * Created on: Dec 20, 2024
+ *
+ * ULID: <sadibos>
+ * Class: IT 179 
+ */
+package personalFinanceManager_PersonalProject1;
+
+/**
+ * <insert class description here>
+ *
+ * @author <Adiboshi Shalom;
+ *
+ */
+public class Main
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
