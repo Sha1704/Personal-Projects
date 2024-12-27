9f1cc05d80cebc89e2862f96f557d156096434b4
Shalom <shalomadiboshi01@gmail.com>
2024-12-26 21:38:58 -0600

    Program Shell

---
+++ taskManager_PersonalProject2/src/module-info.java
@@ -0,0 +1,12 @@
+/*
+ * Created on: Dec 26, 2024
+ *
+ * ULID: <sadibos>
+ * Class: IT 179 
+ */
+/**
+ * 
+ */
+module taskManager_PersonalProject2
+{
+}
---
+++ taskManager_PersonalProject2/src/taskManager_PersonalProject2/Task.java
@@ -0,0 +1,19 @@
+/*
+ * Created on: Dec 26, 2024
+ *
+ * Author: Adiboshi Shalom
+ *
+ *
+ */
+package taskManager_PersonalProject2;
+
+/**
+ * <insert class description here>
+ *
+ * @author <Adiboshi Shalom;
+ *
+ */
+public class Task
+{
+
+}
